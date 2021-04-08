a82dab3a64b7457f6a0936e8b41f587430418d1f
Harik <ovsyanik.garik@gmail.com>
2021-04-08 12:31:33 +0300

    N1 l05

---
+++ js/script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+
+//5 урок
+let number = '9';
+let Name = 'Harik';
+const surname = 'Ovsianyk';
+
+Name = 'Kate';
+
+console.log(Name, surname);
+ 
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+
+console.log(obj);
+
+console.log(name);
+
+var name = 'Harik';
+
+name = 'Kate';
+
+{
+    var result = 50;
+}
+
+
+console.log(result);
+
