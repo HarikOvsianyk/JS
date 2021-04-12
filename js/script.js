3191a4963e80116ca92e1a68d1052ec199c46aa5
Harik <ovsyanik.garik@gmail.com>
2021-04-12 11:13:52 +0300

    N2 lesson 14

--- js/script.js
+++ js/script.js
@@ -102,6 +102,84 @@ const isChecked = !true,
 
 console.log(isChecked || isClose);
 
+// 13 lesson
+
+// Условие If
+
+if (4 !== 9) {
+    console.log('Sure!');
+} else {
+    console.log('Wrong number');
+}
+
+const num = 50;
+
+if (num <= 49) {
+    console.log('Error');
+} else if (num >= 100) {
+    console.log('Too much');
+} else {
+    console.log('Sure!');
+}
+
+// Тернарный оператор (так как участвуют три оператора)
+
+(num === 49) ? console.log('Sure!') : console.log('Error');
+
+//Условие Switch - модификация if, которая поддерживает несколько модификаций и условий
+
+const n = 55;
+
+switch (n) {
+    case 49:
+        console.log('Wrong!');
+        break;
+    case 100:
+        console.log('Wrong number!');
+        break;
+    case 55:
+        console.log('Good job, buddy!');
+        break;
+    default:
+        console.log('Not today =(');
+        break;
+}
 
 
+// 14 Lesson Циклы
 
+// Цикл while
+
+let num50 = 50;
+
+while (num50 <= 55) {
+    console.log(num50);
+    num50++;
+}
+
+//do - сначала что-то делаем, потом проверяем условие и если необходимо - выйти из цикла
+
+let num51 = 51;
+
+do {
+    console.log(num51);
+    num51++;
+}
+while (num51 < 55);
+
+// for - можем подробно его настроить 
+
+let num52 = 52;
+for (let i = 1; i < 8; i++) {
+    console.log(num52);
+    num52++;;
+}
+
+let num5 = 5;
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        /* break; */
+        continue;
+    }
+    console.log(i);
+}
